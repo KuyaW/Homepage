441cc127ae559950a63a68b3a0201ba9c89d420c
KuyaW <91214926+KuyaW@users.noreply.github.com>
2024-04-01 13:11:11 +0800

    Just updated the time.

--- time.js
+++ time.js
@@ -1,16 +1,22 @@
 const fs = require('fs');
 
-// Specify the file name and content
-const fileName = 'date.txt';
-let time = new Date();
-let fileContent = time.toLocaleString();
+
+function time(){
+    // Specify the file name and content
+    const fileName = 'date.txt';
+    let time = new Date();
+    let fileContent = time.toLocaleString();
 
 
-// Create the file
-fs.writeFile(fileName, fileContent, (err) => {
-  if (err) {
-    console.error('Error creating the file:', err);
-  } else {
-    console.log('File created successfully!');
-  }
-});
+    // Create the file
+    fs.writeFile(fileName, fileContent, (err) => {
+    if (err) {
+        console.error('Error creating the file:', err);
+    } else {
+        console.log('File created successfully!');
+    }
+    });
+}
+
+
+module.exports = {time}
---
+++ update.js
@@ -0,0 +1,7 @@
+const update = require('./time');
+const exec = require('./exec');
+
+update.time();
+exec.exec("node .\automate.js");
+
+
