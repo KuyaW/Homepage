bc3eb191f334593d85dc2211699f829ef4808097
KuyaW <91214926+KuyaW@users.noreply.github.com>
2024-04-07 17:54:05 +0800

    Added some feature

--- scripts.js
+++ scripts.js
@@ -9,3 +9,25 @@ function myFunction(username){
 function sayHelloWorld(){
     return "Hello World";
 }
+
+
+
+class Arithmetic{
+    
+    constructor(){
+
+    }
+
+    addition(a, b){
+        return this.a + this.b;
+    }
+
+    subtraction(){
+        return this.a - this.b;
+    }
+}
+
+
+let 
+
+// function 
