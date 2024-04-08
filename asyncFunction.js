dd410c0ef2d963733caf52657c58c652b13485a8
KuyaW <91214926+KuyaW@users.noreply.github.com>
2024-04-08 18:46:58 +0800

    Memorized the format

--- asyncFunction.js
+++ asyncFunction.js
@@ -1,10 +1,8 @@
 function asyncFunction(){
     const http = new XMLHttpRequest();
-
     http.onload = function(){
         document.getElementById('target').innerHTML = this.responseText;
     }
-
     http.open("get", "target.php");
     http.send();
 }
