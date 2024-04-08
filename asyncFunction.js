81f9b4938ac9e09579d32d84dd1519198b4e2c0e
KuyaW <91214926+KuyaW@users.noreply.github.com>
2024-04-08 18:45:14 +0800

    updated the file

--- asyncFunction.js
+++ asyncFunction.js
@@ -1,3 +1,10 @@
 function asyncFunction(){
-    
+    const http = new XMLHttpRequest();
+
+    http.onload = function(){
+        document.getElementById('target').innerHTML = this.responseText;
+    }
+
+    http.open("get", "target.php");
+    http.send();
 }
